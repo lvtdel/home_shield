5b485c73554ce488c9b9c8eb5f5bda4d7edd1405
LVT DEL <votheluc01@gmail.com>
2025-06-19 00:00:26 +0700

    Quick change: Commit app constant

---
+++ lib/core/constant/app_constant.dart
@@ -0,0 +1,10 @@
+class AppConstant {
+  AppConstant._();
+
+  // String mapToken = const String.fromEnvironment("PUBLIC_ACCESS_TOKEN");
+  String mapToken = const String.fromEnvironment("MAP_TOKEN");
+  static String deepAiToken = const String.fromEnvironment("DEEP_AI_TOKEN");
+
+  static String notifImageSample = "https://firebasestorage.googleapis.com/v0/b/home-shield-ce8cb.firebasestorage.app/o/images%2Fgroup%2Fnotification_1540708456-900x900.png?alt=media&token=7c7caf50-a46f-4e46-9ca5-c1b6ed9d08d8";
+  static String avatarSmample = "https://firebasestorage.googleapis.com/v0/b/home-shield-ce8cb.firebasestorage.app/o/images%2Favatar%2FOIP%20(1).jpg?alt=media&token=8ebbf82c-a537-46e9-ba14-340b312acbd6";
+}
